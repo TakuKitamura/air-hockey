26fd87ce3724fda6ea27a083f73dd0a1f5e0b974
Taku Kitamura <tikutaku0128@icloud.com>
2017-07-29 14:49:00 +0900

    計算クラス、追加、修正

--- reinforcement-learning/air-hockey/calculation.cpp
+++ reinforcement-learning/air-hockey/calculation.cpp
@@ -1,89 +1,96 @@
 #include "calculation.h"
 #include <math.h>
+#include <string>
+
+
+using namespace std;
 
 // K = (1/2)mv^2
-double pysicalEnergy( string target, double m, double v ) {
-    return ( 1 / 2 ) * m * ( pow( v, 2.0 ) );
+double Pysics::pysicalEnergy( string target, double m, double v ) {
+    return ( 0.5 ) * m * ( pow( v, 2.0 ) );
 }
 
-double pysicalEnergy( double k, string target, double v ) {
-    return ( 2 * k ) / ( pow( v, 2.0 ) );
+double Pysics::pysicalEnergy( double k, string target, double v ) {
+    return ( 2.0 * k ) / ( pow( v, 2.0 ) );
 }
 
-double pysicalEnergy( double k, double m, string target ) {
-    return sqrt( ( 2 * k ) / m );
+double Pysics::pysicalEnergy( double k, double m, string target ) {
+    return sqrt( ( 2.0 * k ) / m );
 }
 
 // F = μN (動摩擦力)
-double dynamicFriction( string target, double mu, double n ) {
+double Pysics::dynamicFriction( string target, double mu, double n ) {
     return mu * n;
 }
 
-double dynamicFriction( double f, string target, double n ) {
+double Pysics::dynamicFriction( double f, string target, double n ) {
     return f / n;
 }
 
-double dynamicFriction( double f, double mu, string target ) {
+double Pysics::dynamicFriction( double f, double mu, string target ) {
     return f / mu;
 }
 
 
 // v = v_0 + at
-double acceleratedMotionExecptY( string target, double initialV, double a, double t ) {
+double Pysics::acceleratedMotionExceptY( string target, double initialV, double a, double t ) {
     return initialV + ( a * t );
 }
 
-double acceleratedMotionExecptY( double v, string target, double a, double t ) {
+double Pysics::acceleratedMotionExceptY( double v, string target, double a, double t ) {
     return v - ( a * t );
 }
 
-double acceleratedMotionExecptY( double v, double initialV, string target, double t ) {
+double Pysics::acceleratedMotionExceptY( double v, double initialV, string target, double t ) {
     return ( v - initialV ) / t;
 }
 
-double acceleratedMotionExecptY( double v, double initialV, double a, string target ) {
+double Pysics::acceleratedMotionExceptY( double v, double initialV, double a, string target ) {
     return ( v - initialV ) / a;
 }
 
 
 // y  = v_0t + (1/2)at^2
-double acceleratedMotionExceptV( string target, double initialV, double a, double t ) {
-    return initialV + ( ( 1 / 2 ) * a * pow( t, 2.0 ) );
+double Pysics::acceleratedMotionExceptV( string target, double initialV, double a, double t ) {
+    return initialV + ( ( 0.5 ) * a * pow( t, 2.0 ) );
 }
 
-double acceleratedMotionExceptV( double y, string target, double a, double t ) {
-    return  ( (2 * y) - ( a * pow( t, 2.0) ) ) / ( 2 * t )
+double Pysics::acceleratedMotionExceptV( double y, string target, double a, double t ) {
+    return  ( (2 * y) - ( a * pow( t, 2.0) ) ) / ( 2.0 * t );
 }
 
-double acceleratedMotionExceptV( double y, double initialV, string target, double t ) {
-
+double Pysics::acceleratedMotionExceptV( double y, double initialV, string target, double t ) {
     return ( 2 * ( y - ( initialV * t ) ) ) / ( pow( t, 2.0 ) );
 }
 
-double acceleratedMotionExceptV( double y, double initialV, double a, string target ) {
-    return  sqrt( ( 2 * ( y - ( initialV * t ) ) ) / a );
+void Pysics::acceleratedMotionExceptV( double sol[2], double y, double initialV, double a, string target ) {
+    sol[0] = ( - initialV + sqrt( pow( initialV, 2.0 ) + ( 2.0 * a * y ) ) ) / a;
+    sol[1] = ( - initialV - sqrt( pow( initialV, 2.0 ) + ( 2.0 * a * y ) ) ) / a;
 }
 
 
 // v^2 = v_0^2 + 2ay
-double acceleratedMotionExceptT( string target, double initialV, double a, double y ) {
+double Pysics::acceleratedMotionExceptT( string target, double initialV, double a, double y ) {
     return sqrt( pow( initialV, 2.0 ) + ( 2 * a * y ) );
 }
 
-double acceleratedMotionExceptT( double v, string target, double a, double y ) {
+double Pysics::acceleratedMotionExceptT( double v, string target, double a, double y ) {
     return sqrt ( pow( v, 2.0 ) - ( 2 * a * y ) );
 }
 
-double acceleratedMotionExceptT( double v, double initialV, string target, double y ) {
+double Pysics::acceleratedMotionExceptT( double v, double initialV, string target, double y ) {
     return ( ( v + initialV ) * ( v - initialV ) ) / ( 2 * y );
 }
 
-double acceleratedMotionExceptT( double v, double initialV, double a, string target ) {
+double Pysics::acceleratedMotionExceptT( double v, double initialV, double a, string target ) {
     return ( ( v + initialV ) * ( v - initialV ) ) / ( 2 * a );
 }
 
-
 // 弾性衝突後の速さ
-double afterElasticCollisionV( double e, double v ) {
+double Pysics::afterElasticCollisionV( double e, double v ) {
     return -e * v;
+}
+
+double Math::distanceBetweenTwoPoints( double xa, double ya, double xb, double yb ) {
+    return sqrt( pow( xb - xa, 2.0 ) + pow( yb - ya, 2.0 ) );
 }
--- reinforcement-learning/air-hockey/main.cpp
+++ reinforcement-learning/air-hockey/main.cpp
@@ -1,10 +1,38 @@
 #include "calculation.h"
+#include <iostream>
 
 using namespace std;
 
-
 int main(){
+    Pysics phy;
+    Math mat;
 
-  return 0;
+    // cout << phy.pysicalEnergy("x",1.0,1.0) << endl;
+    // cout << phy.pysicalEnergy(1.0,"x",1.0) << endl;
+    // cout << phy.pysicalEnergy(1.0,1.0,"x") << endl;
+
+    // cout << phy.acceleratedMotionExecptY("x",1.0,1.0,1.0) << endl;
+    // cout << phy.acceleratedMotionExecptY(1.0,"x",1.0,1.0) << endl;
+    // cout << phy.acceleratedMotionExecptY(1.0,1.0,"x",1.0) << endl;
+    // cout << phy.acceleratedMotionExecptY(1.0,1.0,1.0,"x") << endl;
+
+    // double sol[2];
+    //
+    // cout << phy.acceleratedMotionExceptV("x",1.0,1.0,1.0) << endl;
+    // cout << phy.acceleratedMotionExceptV(1.0,"x",1.0,1.0) << endl;
+    // cout << phy.acceleratedMotionExceptV(1.0,1.0,"x",1.0) << endl;
+    // phy.acceleratedMotionExceptV(sol,1.0,1.0,1.0,"x");
+    // cout << to_string(sol[0]) + "," + to_string(sol[1]) << endl;
+
+    // cout << phy.acceleratedMotionExceptT("x",1.0,1.0,1.0) << endl;
+    // cout << phy.acceleratedMotionExceptT(1.0,"x",1.0,1.0) << endl;
+    // cout << phy.acceleratedMotionExceptT(1.0,1.0,"x",1.0) << endl;
+    // cout << phy.acceleratedMotionExceptT(1.0,1.0,1.0,"x") << endl;
+
+    // cout << phy.afterElasticCollisionV(0.5,10) << endl;
+
+    cout << mat.distanceBetweenTwoPoints(1,1,2,2) << endl;
+
+    return 0;
 
 }

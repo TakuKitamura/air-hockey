765862a99e4fecc19f680786d1f8adf691eb8ae4
Taku Kitamura <tikutaku0128@icloud.com>
2017-08-03 12:43:04 +0900

    テーブルに摩擦力を追加

--- reinforcement-learning/air-hockey/calculation.cpp
+++ reinforcement-learning/air-hockey/calculation.cpp
@@ -1,94 +1,121 @@
 #include "calculation.h"
 #include <math.h>
 #include <string>
-
+#include <cstdio>
 
 using namespace std;
 
 // K = (1/2)mv^2
-double Pysics::pysicalEnergy( string target, double m, double v ) {
-    return ( 0.5 ) * m * ( pow( v, 2.0 ) );
+double Physics::physicalEnergy( string target, double m, double now_v ) {
+    return ( 0.5 ) * m * ( pow( now_v, 2.0 ) );
 }
 
-double Pysics::pysicalEnergy( double k, string target, double v ) {
-    return ( 2.0 * k ) / ( pow( v, 2.0 ) );
+double Physics::physicalEnergy( double k, string target, double now_v ) {
+    return ( 2.0 * k ) / ( pow( now_v, 2.0 ) );
 }
 
-double Pysics::pysicalEnergy( double k, double m, string target ) {
+double Physics::physicalEnergy( double k, double m, string target ) {
     return sqrt( ( 2.0 * k ) / m );
 }
 
 // F = μN (動摩擦力)
-double Pysics::dynamicFriction( string target, double mu, double n ) {
+double Physics::dynamicFriction( string target, double mu, double n ) {
     return mu * n;
 }
 
-double Pysics::dynamicFriction( double f, string target, double n ) {
+double Physics::dynamicFriction( double f, string target, double n ) {
     return f / n;
 }
 
-double Pysics::dynamicFriction( double f, double mu, string target ) {
+double Physics::dynamicFriction( double f, double mu, string target ) {
     return f / mu;
 }
 
+double Physics::afterSpeedOnFriction ( string target, double now_v ) {
+    
+    // 平方根が存在しない時
+    if( 2 * MU * G  > pow( now_v, 2 ) ) {
+        // printf("NO SQUARE ROOT!\n");
+        return 0.0;
+    }
+
+    return sqrt( pow( now_v, 2 ) - ( 2 * MU * G ) );
+}
+
+// 変化前の運動エネルギー = 現在の運動エネルギー + 外力が物体にした仕事
+// double calculationDoneWork( string target, double now_physical_energy, double outside_energy ) {
+//     return now_physical_energy + outside_energy;
+// }
+//
+// double calculationDoneWork( double before_physical_energy, string target, double outside_energy ) {
+//     return before_physical_energy - outside_energy;
+// }
+
+// double calculationDoneWork( double before_physical_energy, double now_physical_energy, string target ) {
+//     return before_physical_energy - now_physical_energy;
+// }
+
+// double calculationAfterSpeed ( string target, double now_v ) {
+//     return sqrt ( pow( now_v, 2 ) - ( 2 * MU * G ) );
+// }
 
 // v = v_0 + at
-double Pysics::acceleratedMotionExceptY( string target, double initialV, double a, double t ) {
+double Physics::acceleratedMotionExceptY( string target, double initialV, double a, double t ) {
     return initialV + ( a * t );
 }
 
-double Pysics::acceleratedMotionExceptY( double v, string target, double a, double t ) {
-    return v - ( a * t );
+double Physics::acceleratedMotionExceptY( double now_v, string target, double a, double t ) {
+    return now_v - ( a * t );
 }
 
-double Pysics::acceleratedMotionExceptY( double v, double initialV, string target, double t ) {
-    return ( v - initialV ) / t;
+double Physics::acceleratedMotionExceptY( double now_v, double initialV, string target, double t ) {
+    return ( now_v - initialV ) / t;
 }
 
-double Pysics::acceleratedMotionExceptY( double v, double initialV, double a, string target ) {
-    return ( v - initialV ) / a;
+double Physics::acceleratedMotionExceptY( double now_v, double initialV, double a, string target ) {
+    return ( now_v - initialV ) / a;
 }
 
 
 // y  = v_0t + (1/2)at^2
-double Pysics::acceleratedMotionExceptV( string target, double initialV, double a, double t ) {
+double Physics::acceleratedMotionExceptV( string target, double initialV, double a, double t ) {
     return initialV + ( ( 0.5 ) * a * pow( t, 2.0 ) );
 }
 
-double Pysics::acceleratedMotionExceptV( double y, string target, double a, double t ) {
+double Physics::acceleratedMotionExceptV( double y, string target, double a, double t ) {
     return  ( (2 * y) - ( a * pow( t, 2.0) ) ) / ( 2.0 * t );
 }
 
-double Pysics::acceleratedMotionExceptV( double y, double initialV, string target, double t ) {
+double Physics::acceleratedMotionExceptV( double y, double initialV, string target, double t ) {
     return ( 2 * ( y - ( initialV * t ) ) ) / ( pow( t, 2.0 ) );
 }
 
-void Pysics::acceleratedMotionExceptV( double sol[2], double y, double initialV, double a, string target ) {
+void Physics::acceleratedMotionExceptV( double sol[2], double y, double initialV, double a, string target ) {
     sol[0] = ( - initialV + sqrt( pow( initialV, 2.0 ) + ( 2.0 * a * y ) ) ) / a;
     sol[1] = ( - initialV - sqrt( pow( initialV, 2.0 ) + ( 2.0 * a * y ) ) ) / a;
 }
 
 
 // v^2 = v_0^2 + 2ay
-double Pysics::acceleratedMotionExceptT( string target, double initialV, double a, double y ) {
+double Physics::acceleratedMotionExceptT( string target, double initialV, double a, double y ) {
     return sqrt( pow( initialV, 2.0 ) + ( 2 * a * y ) );
 }
 
-double Pysics::acceleratedMotionExceptT( double v, string target, double a, double y ) {
-    return sqrt ( pow( v, 2.0 ) - ( 2 * a * y ) );
+double Physics::acceleratedMotionExceptT( double now_v, string target, double a, double y ) {
+    return sqrt ( pow( now_v, 2.0 ) - ( 2 * a * y ) );
 }
 
-double Pysics::acceleratedMotionExceptT( double v, double initialV, string target, double y ) {
-    return ( ( v + initialV ) * ( v - initialV ) ) / ( 2 * y );
+double Physics::acceleratedMotionExceptT( double now_v, double initialV, string target, double y ) {
+    return ( ( now_v + initialV ) * ( now_v - initialV ) ) / ( 2 * y );
 }
 
-double Pysics::acceleratedMotionExceptT( double v, double initialV, double a, string target ) {
-    return ( ( v + initialV ) * ( v - initialV ) ) / ( 2 * a );
+double Physics::acceleratedMotionExceptT( double now_v, double initialV, double a, string target ) {
+    return ( ( now_v + initialV ) * ( now_v - initialV ) ) / ( 2 * a );
 }
 
 // 弾性衝突後の速さ
-double Pysics::afterElasticCollisionV( double e, double v ) {
-    return -e * v;
+double Physics::afterElasticCollisionV( double now_v, double e ) {
+    return -e * now_v;
 }
 
 double Math::distanceBetweenTwoPoints( double xa, double ya, double xb, double yb ) {
@@ -99,11 +126,46 @@ double Math::innerProduct( double xa, double ya, double xb, double yb ) {
     return ( xa * xb ) + ( ya * yb );
 }
 
+// 0° <= θ <= 90°
 double Math::formedAngle( double xa, double ya, double xb, double yb ) {
-    return cos(
+
+    double formed_angle = cos(
         Math::innerProduct( xa, ya, xb, yb ) /
             ( Math::distanceBetweenTwoPoints( xa , xb , 0 , 0 ) *
             Math::distanceBetweenTwoPoints( 0 , 0 , xb , xb )
             )
     );
+
+    return formed_angle < 0 ? -formed_angle : formed_angle;
+}
+
+double Math::syntheticVector ( double a, double b ) {
+    return sqrt( pow( a, 2.0 ) + pow( b, 2.0 ) );
+
+}
+
+void Math::resolutionVector ( double vector_size, double cos_theta, double v[2], string axis) {
+
+    // printf("vector_size = %f, cos_theta = %f, v[0] = %f, v[1] = %f\n", vector_size, cos_theta, v[0], v[1]);
+
+    v[0] = v[0] < 0 ? v[0] * -1.0 : v[0];
+    v[1] = v[1] < 0 ? v[1] * -1.0 : v[1];
+    // printf("sin = %f, cos = %f", 1.0 - pow( cos_theta, 2.0 ), cos_theta);
+    // if( axis == "x" ) {
+    //     v[0] = vector_size * cos_theta;
+    //     v[1] = vector_size * sqrt ( 1.0 - pow( cos_theta, 2.0 ) );
+    // }
+    //
+    // else {
+    //     v[0] = vector_size * sqrt ( 1.0 - pow( cos_theta, 2.0 ) );
+    //     v[1] = vector_size * cos_theta;
+    // }
+
+    v[0] = vector_size * cos_theta;
+    v[1] = vector_size * sqrt ( 1.0 - pow( cos_theta, 2.0 ) );
+
+
+
+
+
 }
--- reinforcement-learning/air-hockey/main.cpp
+++ reinforcement-learning/air-hockey/main.cpp
@@ -9,79 +9,148 @@
 #define LENGTH 732
 #define OX 194
 #define OY 366
+#define TIME 1
 
 
 double before_pack_pos[2] = { 0, 0 };
 double pack_pos[2] = { 0, 0 };
-double a[2] = { 0.5, 0.5 };
-double v_direction[2] = { 0.0, 0.0 };
+double now_v[2] = { 0.5, 0.5 };
 
 Math math;
+Physics phy;
 
 void launchPack( double pack_pos[2], double a[2] ){
 
     srand( ( unsigned ) time( NULL ) );
     pack_pos[0] = rand() % (OX - 48) + 24;
     pack_pos[1] = rand() % (OY - 48) + 24;
-    printf("%f",pack_pos[1]);
+    // printf("%f",pack_pos[1]);
     //
     pack_pos[0] = rand() % 2 ? pack_pos[0] : -pack_pos[0];
     // pack_pos[1] = rand() % 2 ? pack_pos[1] : -pack_pos[1];
     //
-    a[0] = rand() % 10 + 1;
-    a[1] = rand() % 10 + 1;
+    now_v[0] = rand() % 10 + 10;
+    now_v[1] = rand() % 10 + 10;
     //
-    a[0] = rand() % 2 ? a[0] : -a[0];
-    // a[1] = rand() % 2 ? a[1] : -a[1];
+    now_v[0] = rand() % 2 ? now_v[0] : -now_v[0];
+    // now_v[1] = rand() % 2 ? now_v[1] : -now_v[1];
     //
-    // a[0] *= -1;
-    a[1] *= -1;
+    // now_v[0] *= -1;
+    now_v[1] *= -1;
 
     // pack_pos[1] = 100;
-    // a[1] = 1;
-    // a[0] = -100;
-    // a[1] = -100;
+    // now_v[1] = 1;
+    // now_v[0] = -100;
+    // now_v[1] = -100;
 
-    printf("pack_pos[0] = %f, pack_pos[1] = %f, a[0] = %f, a[1] = %f\n",pack_pos[0],pack_pos[1],a[1],a[2]);
+    // printf("pack_pos[0] = %f, pack_pos[1] = %f, now_v[0] = %f, now_v[1] = %f\n",pack_pos[0],pack_pos[1],now_v[1],a[2]);
 
 }
 
 void movePack( double pack_pos[2] , double before_pack_pos[2]) {
 
-
     int quadrant = 0;
+
+    // double now_physical_energy = 0.0;
+    // double after_physical_energy = 0.0;
+    double outside_energy = 0.0;
+
+    // double formed_angle_x = 0.0;
+    // double formed_angle_y = 0.0;
+    double formed_angle = 0.0;
+
+    double before_synthetic_v = 0.0;
+    double after_synthetic_v = 0.0;
+
+    double now_v_sign[2] = { 0.0, 0.0 };
+
+
+    // double direction[2] = { 0.0, 0.0 };
+
+    double dynamic_friction = phy.dynamicFriction( "F", MU, PM * G );
 
     before_pack_pos[0] = pack_pos[0];
     before_pack_pos[1] = pack_pos[1];
 
-    pack_pos[0] = pack_pos[0] + a[0];
-    pack_pos[1] = pack_pos[1] + a[1];
+    pack_pos[0] = pack_pos[0] + now_v[0];
+    pack_pos[1] = pack_pos[1] + now_v[1];
 
 
-    if ( pack_pos[0] > OX - 24 ) {
+    if( pack_pos[0] > OX - 24 ) {
         pack_pos[0] = OX - 24;
     }
 
-    else if ( pack_pos[0] < -(OX - 24) ) {
+    else if( pack_pos[0] < -(OX - 24) ) {
         pack_pos[0] = -( OX - 24 );
     }
 
-    if ( pack_pos[1] > OY - 24 ) {
+    if( pack_pos[1] > OY - 24 ) {
         pack_pos[1] = OY - 24;
     }
 
-    else if ( pack_pos[1] < -(OY - 24) ) {
+    else if( pack_pos[1] < -(OY - 24) ) {
         pack_pos[1] = -( OY - 24 );
     }
 
-    v_direction[0] = pack_pos[0] - before_pack_pos[0];
-    v_direction[1] = pack_pos[1] - before_pack_pos[1];
+    // direction[0] = before_pack_pos[0] - pack_pos[0];
+    // direction[1] = before_pack_pos[1] - pack_pos[1];
 
-    a[0] = (pack_pos[0] == OX - 24 || pack_pos[0] == -( OX - 24 ) ) ? -a[0] * E : a[0];
-    a[1] = (pack_pos[1] == OY - 24 || pack_pos[1] == -( OY - 24 ) ) ? -a[1] * E : a[1];
+    before_synthetic_v = math.syntheticVector( now_v[0], now_v[1] );
 
-    // printf("a[0] * 0.75 = %f, a[1] * 0.75 = %f\n",-a[0] * E,-a[1] * E);
-    // printf("a[0] = %f, a[1] = %f\n",a[0],a[1]);
+    // now_physical_energy = phy.physicalEnergy( "K", PM, before_synthetic_v );
+
+    if( pack_pos[0] == OX - 24 || pack_pos[0] == -( OX - 24 ) ) {
+        now_v[0] = phy.afterElasticCollisionV( now_v[0], E );
+    }
+
+    if( pack_pos[1] == OY - 24 || pack_pos[1] == -( OY - 24 ) ) {
+        now_v[1] = phy.afterElasticCollisionV( now_v[1], E );
+    }
+
+    now_v_sign[0] = now_v[0] > 0 ? 1.0 : -1.0;
+    now_v_sign[1] = now_v[1] > 0 ? 1.0 : -1.0;
+
+    // formed_angle_x = math.formedAngle( OX, 0, pack_pos[0], pack_pos[1] );
+    // formed_angle_y = math.formedAngle( 0, OY, pack_pos[0], pack_pos[1] );
+
+    formed_angle = math.formedAngle( OX, 0, pack_pos[0], pack_pos[1] );;
+
+    // printf("formed_angle = %f\n",formed_angle);
+
+    // printf("1: now_v[0] = %f, now_v[1] = %f\n", now_v[0], now_v[1]);
+
+    // after_physical_energy = phy.physicalEnergy( "K", PM, before_synthetic_v );
+
+    after_synthetic_v = phy.afterSpeedOnFriction( "v", before_synthetic_v );
+
+    // printf("%f\n",after_synthetic_v);
+
+
+
+    math.resolutionVector( after_synthetic_v, formed_angle, now_v, "x" );
+
+    // math.resolutionVector( after_synthetic_v, formed_angle_y , now_v, "y" );
+
+    // formed_angle_y == 0.0 ?
+    //          :
+
+
+    now_v[0] *= now_v_sign[0];
+    now_v[1] *= now_v_sign[1];
+
+    // printf("2: now_v[0] = %f, now_v[1] = %f\n\n", now_v[0], now_v[1]);
+
+
+    // now_v[1] = phy.afterSpeedOnFriction( "v'_y",
+
+
+
+
+    // now_v[0] = (pack_pos[0] == OX - 24 || pack_pos[0] == -( OX - 24 ) ) ? -now_v[0] * E : now_v[0];
+    // now_v[1] = (pack_pos[1] == OY - 24 || pack_pos[1] == -( OY - 24 ) ) ? -now_v[1] * E : now_v[1];
+
+    // printf("now_v[0] * 0.75 = %f, now_v[1] * 0.75 = %f\n",-now_v[0] * E,-now_v[1] * E);
+    // printf("now_v[0] = %f, now_v[1] = %f\n",now_v[0],now_v[1]);
     // printf("move_dist[0] = %f, move_dist[1] = %f\n",move_dist[0],move_dist[1]);
     // printf("before_pack_pos[0] = %f, before_pack_pos[1] = %f\n",before_pack_pos[0],before_pack_pos[1]);
     // printf("v_direction[0] = %f, v_direction[1] = %f\n",v_direction[0],v_direction[1]);
@@ -90,6 +159,8 @@ void movePack( double pack_pos[2] , double before_pack_pos[2]) {
     // printf("pack_pos[0] = %f, pack_pos[1] = %f",pack_pos[0],pack_pos[1]);
 
 }
+
+
 
 void display(void)
 {
@@ -125,8 +196,8 @@ void display(void)
 
     for (i = 0; i < n; i++) {
 
-        x = r * cos(2.0 * 3.14 * ((double)i/n) );
-        y = r * sin(2.0 * 3.14 * ((double)i/n) );
+        x = r * cos(2.0 * PI * ((double)i/n) );
+        y = r * sin(2.0 * PI * ((double)i/n) );
 
         // printf("x = %lf, y = %lf\n",x,y);
 
@@ -202,7 +273,7 @@ void mouse(int button, int state, int x, int y)
 void timer( int value ) {
     movePack( pack_pos, before_pack_pos );
     glutPostRedisplay();
-    glutTimerFunc( 1, timer, 0 );
+    glutTimerFunc( TIME, timer, 0 );
 
 }
 
@@ -218,8 +289,8 @@ int main(int argc, char *argv[])
     glutReshapeFunc(resize);
     // glutMouseFunc(mouse);
     init();
-    launchPack( pack_pos, a );
-    glutTimerFunc( 1, timer, 0 );
+    launchPack( pack_pos, now_v );
+    glutTimerFunc( TIME, timer, 0 );
     glutMainLoop();
 
 

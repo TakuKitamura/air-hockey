fff799f1adcacb7e4c2f0a225316b9a4db0c4e8b
喜多村卓 <is0352pp@ed.ritsumei.ac.jp>
2017-10-05 21:54:29 +0900

    不要ファイルを削除

--- monte_carlo_method.cpp
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-
-double MonteCarloMethod::upperConfidenceBound(
-    double this_method_winning_percentage,
-    int total_method_counts,
-    int this_method_study_counts,
-    double constant ) {
-        return this_method_winning_percentage + ( constant * ( sqrt( log( total_method_counts ) / this_method_study_counts ) ) );
-}
